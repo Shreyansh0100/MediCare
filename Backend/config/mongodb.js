08f186d678a64c40bd6ef529c51a1631b15480c4
Shreyansh0100 <shreyanshkumar010@gmail.com>
2026-06-06 18:11:50 +0530

    Update error

--- Backend/config/mongodb.js
+++ Backend/config/mongodb.js
@@ -3,6 +3,6 @@ const connectDB=async()=>{
     mongoose.connection.on("connected",()=>
         console.log("Database connected")
     )
-    await mongoose.connect(`${process.env.MONGODB_URI}/prescripto`)
+await mongoose.connect(process.env.MONGODB_URI)
 }
 export default connectDB
